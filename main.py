a29b8c0bc26100d46ccb9d5df7545821d2a89aaf
refkyf12 <refky.f1212@gmail.com>
2023-10-11 10:57:35 +0700

    Simplifikasi

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 from flask import Flask, render_template, Response
-from stream import streamer, streamer2
+from stream import streamer
 
 app = Flask(__name__)
+CAMERAS = [
+   (1, "tcp://localhost:5555"),
+   (2, "tcp://localhost:6666")
+]
 
-def gen():
+def gen(id):
+  print([item[1] for item in CAMERAS if item[0] == int(id)])
   while True:
-      stream = streamer()
-      yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + stream + b'\r\n\r\n')
-
-def gen2():
-   while True:
-      stream = streamer2()
+      stream = streamer([item[1] for item in CAMERAS if item[0] == int(id)][0])
       yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + stream + b'\r\n\r\n')
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/camera')
-def camera():
-  return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/camera2')
-def camera2():
-  return Response(gen2(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/stream/<id>')
+def camera(id):
+  return Response(gen(id), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
